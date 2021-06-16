45b89c88cde79ceffa8310a20a99ccdb7c4cb480
Andrew Levine <levine.andrew@heb.com>
2021-06-16 11:00:18 -0500

    Pair programming updates

--- screens/Confirm.js
+++ screens/Confirm.js
@@ -1,43 +1,44 @@
-import React from "react";
+import React from 'react';
 
-import { Container, TextH5 } from "../UI";
+import { Container, TextH5 } from '../UI';
 
-export default function ConfirmScreen(props){
-   console.log(props.route.params)
-  //  console.log(props.route.params.data)
-  //  console.log(Object.keys(props.route.params))
-  // const data = props.route.params.data;
-  // const data = props.route.data;
+export default function ConfirmScreen(props) {
+    console.log(props.route.params);
 
-  //  const data = props.route.params.data ?? 'NO QR';
-  // const data = Object.keys(props.route.params).map(i => {
-  //   return({i.map(j => {
-  //     return (
-  //       <h1>j</h1>)
-  //   }
-  //    )})
-  // })
-  const data = Object.keys(props.route.params).map(data => data.concat())
+    React.useEffect(() => {
+        console.log('Confirm component mounted');
+    }, []);
+    //  console.log(props.route.params.data)
+    //  console.log(Object.keys(props.route.params))
+    // const data = props.route.params.data;
+    // const data = props.route.data;
 
+    //  const data = props.route.params.data ?? 'NO QR';
+    // const data = Object.keys(props.route.params).map(i => {
+    //   return({i.map(j => {
+    //     return (
+    //       <h1>j</h1>)r
+    //   }
+    //    )})
+    // })
+    const data = Object.keys(props.route.params).map((data) => data.concat());
 
-  //  const data = props.route.params
-  // const data = props.route.params?.data;
+    //  const data = props.route.params
+    // const data = props.route.params?.data;
 
-
-  return (
-    <Container>
-      <TextH5>{data}</TextH5>
-      {/* {
+    return (
+        <Container>
+            <TextH5>{data}</TextH5>
+            {/* {
           for (var i = 0; i < data.length; i++) {
             data.push(
 
             )
           }
       } */}
-
-    </Container>
-  );
+        </Container>
+    );
 }
 ConfirmScreen.navigationOptions = {
-  title: 'Confirm'
+    title: 'Confirm',
 };
--- screens/ScannerScreen.js
+++ screens/ScannerScreen.js
@@ -1,112 +1,113 @@
-
-
 import React from 'react';
 
-
-import { Container, Spinner, TextH3 } from "../UI";
+import { Container, Spinner, TextH3 } from '../UI';
 
 import * as Permissions from 'expo-permissions';
 
 import { BarCodeScanner } from 'expo-barcode-scanner';
 
-import layout from "../constants/Layout";
+import layout from '../constants/Layout';
 const { window } = layout;
 
-class ScannerScreen extends React.Component{
-  static navigationOptions = {
-    header: null
-  }
-  // Component State
-  state = {
-    hasCameraPermission: null, // if app has permissions to acess camera
-    isScanned: false, // scanned
-    totalData: []
-  }
-  async componentDidMount() {
-    // ask for camera permission
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    console.log(status);
-    this.setState({ hasCameraPermission: status === "granted" ? true : false });
-  }
+class ScannerScreen extends React.Component {
+    static navigationOptions = {
+        header: null,
+    };
+    // Component State
+    state = {
+        hasCameraPermission: null, // if app has permissions to acess camera
+        isScanned: false, // scanned
+        totalData: [],
+    };
+    async componentDidMount() {
+        this.props.navigation.addListener('focus', () =>
+            this.setState({ isScanned: false }),
+        );
 
+        // ask for camera permission
+        const { status } = await Permissions.askAsync(Permissions.CAMERA);
+        console.log(status);
+        this.setState({
+            hasCameraPermission: status === 'granted' ? true : false,
+        });
+    }
 
-  handleBarCodeScanned = ({ type, data }) => {
-      // Do something here
-      this.setState({isScanned: true});
-      this.props.navigation.navigate('Root', {
-        screen: 'Confirm',
-        params: {
-          screen: 'Confirm',
-          params: data
+    handleBarCodeScanned = ({ type, data }) => {
+        // Do something here
+        this.setState({ isScanned: true });
+        // we're navigating to the wrong spot here
+        this.props.navigation.navigate('Root', {
+            screen: 'Confirm',
+            params: {
+                screen: 'Confirm',
+                params: data,
+            },
+        });
+
+        //   this.props.navigation.navigate('Root', {
+        //     screen: 'Decode',
+        //     params: {
+        //       screen: 'Decode',
+        //       params: totalData
+        //     }
+        //   });
+    };
+    render() {
+        console.log(this.props);
+        console.log(this.state);
+        const { hasCameraPermission, isScanned } = this.state;
+
+        // for(let i = 0; i < totalData.length; i++){
+        //     totalData.push(
+        //         <View key = {i}>
+        //             <p>test</p>
+        //             <TextInput />
+        //         </View>
+        //     )
+        // }
+
+        if (hasCameraPermission === null) {
+            console.log('Requesting permission');
+            return <Spinner />;
         }
-      });
 
-    //   this.props.navigation.navigate('Root', {
-    //     screen: 'Decode',
-    //     params: {
-    //       screen: 'Decode',
-    //       params: totalData
-    //     }
-    //   });
-  }
-  render(){
-      console.log(this.props)
-      console.log(this.state)
-    const { hasCameraPermission, isScanned } = this.state;
-
-    // for(let i = 0; i < totalData.length; i++){
-    //     totalData.push(
-    //         <View key = {i}>
-    //             <p>test</p>
-    //             <TextInput />
-    //         </View>
-    //     )
-    // }
-
-    if(hasCameraPermission === null){
-      console.log("Requesting permission");
-      return (
-        <Spinner />
-      );
+        if (hasCameraPermission === false) {
+            return (
+                <Container>
+                    <TextH3>Please grant Camera permission</TextH3>
+                </Container>
+            );
+        }
+        if (
+            hasCameraPermission === true &&
+            !isScanned &&
+            this.props.navigation.isFocused()
+        ) {
+            return (
+                <Container
+                    style={{
+                        flex: 1,
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <TextH3>Scan code inside window</TextH3>
+                    <BarCodeScanner
+                        onBarCodeScanned={
+                            isScanned ? undefined : this.handleBarCodeScanned
+                        }
+                        style={{
+                            height: window.height / 2,
+                            width: window.height,
+                        }}
+                    ></BarCodeScanner>
+                </Container>
+            );
+        } else {
+            return <Spinner />;
+        }
     }
-
-    if(hasCameraPermission === false){
-      return ( 
-        <Container>
-         <TextH3>Please grant Camera permission</TextH3>
-        </Container> 
-      )
-    }
-    if(hasCameraPermission === true && !isScanned && this.props.navigation.isFocused() ){
-      return <Container style = {{
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center'
-
-      }}>
-          
-        <TextH3>Scan code inside window</TextH3>
-        <BarCodeScanner
-          onBarCodeScanned = { isScanned ? undefined : this.handleBarCodeScanned }
-          style = {{
-            height:  window.height / 2,
-            width: window.height,
-          }}
-        >
-        </BarCodeScanner>
-      </Container>
-    }
-    else{
-      return <Spinner />;
-    }
-
-
-  }
-
 }
-
-
-
 
 export default ScannerScreen;
