63c08c95c1a51e0a2c9f372349425a6ea0cf26d2
Kaylee Su <su.kaylee@heb.com>
2021-06-21 19:08:01 -0500

    added background img

--- screens/Home.js
+++ screens/Home.js
@@ -1,16 +1,42 @@
 import React from 'react';
-import { View, Text } from "react-native";
+import { ImageBackground, StyleSheet, View, Text } from "react-native";
 
-
+// const image = { uri: 'https://reactjs.org/logo-og.png' };
 
 class Home extends React.Component {
     render() {
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
+              {/* <ImageBackground source={image} style={styles.image}>
+              <Text style={styles.text}>Insidegh</Text>
+                </ImageBackground> */}
+                <ImageBackground
+                    source= {require('../assets/images/HomePg-icons.png')}
+                    style={{width: '100%', height: '100%'}} >
+                    </ImageBackground>
+
+            {/* <Text>Home Screen</Text> */}
+            
           </View>
         )
     }
 }
+
+// const styles = StyleSheet.create({
+//     container: {
+//       flex: 1,
+//       flexDirection: 'column',
+//     },
+//     image: {
+//       flex: 1,
+//       resizeMode: 'cover',
+//       justifyContent: 'center',
+//     },
+//     text: {
+//       color: 'grey',
+//       fontSize: 30,
+//       fontWeight: 'bold',
+//     },
+//   });
 
 export default Home;
